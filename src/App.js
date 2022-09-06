ed6bf735c27195883c37fa1c1f68b4c8737c2938
joey <joeychong0213@gmail.com>
2022-09-06 21:49:40 +0800

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
 import ValidateEmail from "./components/ValidateEmail";
+import "bootstrap/dist/css/bootstrap.min.css";
+import image from "./images/exabytes-removebg-preview.png";
+
 
 function App() {
-    // const showEmail = () => {};
+  // const showEmail = () => {};
 
-    return (
-        <div className="py-5 text-white">
-            <div className="container w-75 mb-5">
-                <h1 className="fw-bold text-center">Email Validator</h1>
-                <h4 className="text-center">
-                    Afraid of receiving malicious emails from suspicious
-                    senders?
-                </h4>
-                <h4 className="text-center">
-                    Fret not! With just a click, the email will either be
-                    authentic or a fake.
-                </h4>
-            </div>
-            <div className="container w-50">
-                <div>
-                    <ValidateEmail />
-                </div>
-            </div>
+  return (
+    <div className="Maincontainer py-5 text-white">
+      <div className="mb-5">
+        <h1 className="fw-bold text-center">
+          <img src={image} width="100px" />
+          <span style={{ marginLeft: "-13px" }}>mail Validator</span>
+        </h1>
+        <p className="text-center ">
+          Afraid of receiving malicious emails from suspicious senders?
+          <br />
+          Fret not! With just a click, the email will either be authentic or a
+          fake.
+        </p>
+      </div>
+      <div className="container">
+        <div>
+          <ValidateEmail />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ValidateEmail.js
+++ src/components/ValidateEmail.js
@@ -1,44 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, InputGroup, Button } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 const ValidateEmail = () => {
-    const validRegEx = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    /*
+  const [text, setText] = useState({});
+
+  const validRegEx = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  /*
         \w same as [a-zA-Z0-9_]
         ^ indicates the beginning of the regex
         $ indicates the end of the regex
     */
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
-        const textInput = document.getElementById("email");
-        const email = textInput.value;
+  const onChangeHandler = (e) => {
+    setText({ ...text, [e.target.name]: e.target.value });
+  };
+  console.log(text);
 
-        if (email.match(validRegEx)) alert("It's a valid email");
-        else alert("It's an invalid email");
-    };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
 
-    return (
-        <div>
-            <form action="" method="POST" onSubmit={onSubmitHandler}>
-                <div class="form-group">
-                    <label for="email">Insert email address here</label>
-                    <input
-                        type="email"
-                        class="form-control"
-                        id="email"
-                        placeholder="example@email.com"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    style={{ backgroundColor: "#EF8275" }}
-                    className="btn"
-                >
-                    Check Email
-                </button>
-            </form>
-        </div>
+    if (text.email.match(validRegEx))
+      Swal.fire({
+        icon: "success",
+        title: "It's a valid email",
+        confirmButtonColor: "blue"
+      }
     );
+    else Swal.fire({
+      icon: "error",
+      title: "It's an invalid email",
+      confirmButtonColor: "blue"
+    })
+  };
+
+  return (
+    <div>
+      <form action="" method="POST" onSubmit={onSubmitHandler}>
+        <InputGroup className="mb-3">
+          <Form.Control
+            type="text"
+            className="form-control"
+            name="email"
+            placeholder="example@email.com"
+            onChange={onChangeHandler}
+          />
+          <Button variant="primary" id="button-addon2" type="submit">
+            <i className="bi bi-search "></i>
+          </Button>
+        </InputGroup>
+      </form>
+    </div>
+  );
 };
 
 export default ValidateEmail;
